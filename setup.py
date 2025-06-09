b4f2f74b354cb00e41b0079825606865c6245595
averageandyyy <averageandyyy@gmail.com>
2025-06-09 10:33:37 +0800

    add models

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'models'), )
+        (os.path.join('share', package_name, 'models'), glob(os.path.join("models", "to_use", "*.pth")))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
