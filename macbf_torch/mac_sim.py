33e861d23027fc51c91237587cec53aadc8896d8
averageandyyy <averageandyyy@gmail.com>
2025-06-09 14:09:12 +0800

    use clock

--- macbf_torch/mac_sim.py
+++ macbf_torch/mac_sim.py
@@ -217,6 +217,7 @@ class MACSimNode(Node):
         self.state.quat.y = current_state_vector[7].item()
         self.state.quat.z = current_state_vector[8].item()
         self.state.quat.w = current_state_vector[9].item()
+        self.state.header.stamp = self.get_clock().now()
         # self.get_logger().info(f'Publishing state: {self.state}')
         self.state_publisher.publish(self.state)
 
