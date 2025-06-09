cc3b351c17f0c735ca8d2f0512d6b49db682402b
averageandyyy <averageandyyy@gmail.com>
2025-06-09 11:14:18 +0800

    change setup to contain launch file

--- setup.py
+++ setup.py
@@ -12,7 +12,11 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'models'), glob(os.path.join("models", "to_use", "*.pth")))
+        (os.path.join('share', package_name, 'models'), glob(os.path.join("models", "to_use", "*.pth"))),
+        (
+            os.path.join("share", package_name, "launch"),
+            glob(os.path.join("launch", "*launch.[pxy][yma]*")),
+        ),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
